0a0522fbbf3ee0a5574c83a267909a43df56319d
Dhiraj jaware <dhiraj.javare@gmail.com>
2023-04-06 18:06:03 +0530

    initial commit

---
+++ javascript/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const containerImage = document.querySelector('.row');
+
+// containerImage.addEventListener('mouseover', function (e) {
+//   const img = e.target.closest('img');
+
+//   if (!img) return;
+
+// });
